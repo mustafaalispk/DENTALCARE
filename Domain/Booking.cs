37c59e775e077bc650b024ec723988354ecd6582
Mustafa Ali <mustafaalispk@yahoo.com>
2020-08-01 23:16:40 +0200

    Add class Booking

---
+++ Domain/Booking.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DentalCare.Domain
+{
+    class Booking
+    {
+        public Booking(string firstName, string lastName, string socialSecuirtyNumber, string phoneNumber, string email, string reason, string employee, DateTime date)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecuirtyNumber = socialSecuirtyNumber;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Reason = reason;
+            Employee = employee;
+            Date = date;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string SocialSecuirtyNumber { get; }
+        public string PhoneNumber { get; }
+        public string Email { get; }
+        public string Reason { get; }
+        public string Employee { get; }
+        public DateTime Date { get; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using DentalCare.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading;
 using static System.Console;
 
 namespace DentalCare
 {
     class Program
     {
+        static List<Booking> bookingList = new List<Booking>();
+
         static void Main(string[] args)
         {
+           
             bool shouldNotExit = true;
 
             while (shouldNotExit)
@@ -24,46 +30,93 @@ namespace DentalCare
                     case ConsoleKey.D1:
                     case ConsoleKey.NumPad1:
 
-                        Write("Förnamn: ");
-                        string firstName = ReadLine();
+                        bool isBooked = false;
+
+                        do
+                        {
+                            Clear(); 
+
+                            Write("Förnamn: ");
+                            string firstName = ReadLine();
 
 
-                        Write("Efternam: ");
-                        string lastName = ReadLine();
+                            Write("Efternam: ");
+                            string lastName = ReadLine();
 
 
-                        Write("Personnummer: ");
-                        string socialSecuirtyNumber = ReadLine();
+                            Write("Personnummer: ");
+                            string socialSecuirtyNumber = ReadLine();
 
 
-                        Write("Telefonummer: ");
-                        string phoneNumber = ReadLine();
+                            Write("Telefonummer: ");
+                            string phoneNumber = ReadLine();
 
 
-                        Write("E-mail: ");
-                        string email = ReadLine();
+                            Write("E-mail: ");
+                            string email = ReadLine();
 
 
-                        Write("Anledning: ");
-                        string reason = ReadLine();
+                            Write("Anledning: ");
+                            string reason = ReadLine();
 
 
-                        Write("Utförs av: ");
-                        string employee = ReadLine();
+                            Write("Utförs av: ");
+                            string employee = ReadLine();
 
 
-                        Write("Datum (yyyy-mm-dd hh:mm): ");
-                        DateTime date = DateTime.Parse(ReadLine());
+                            Write("Datum (yyyy-mm-dd hh:mm): ");
+                            DateTime date = DateTime.Parse(ReadLine());
 
-                        WriteLine("Är dettta rätt ? (J)a eller (N)ej");
-                        
-                        // Vi skriver true därför att vi vill inte att tecknad skriver upp på skärmen.
-                        keyPressed = ReadKey(true);
+                            WriteLine("Är dettta rätt ? (J)a eller (N)ej");
+
+                            bool validKeyPressed;
+
+                            do
+                            {
+                                // Vi skriver true därför att vi vill inte att tecknad skriver upp på skärmen.
+                                keyPressed = ReadKey(true);
+
+                                validKeyPressed = keyPressed.Key == ConsoleKey.J ||
+                                    keyPressed.Key == ConsoleKey.N;
+
+                            } 
+                            while (!validKeyPressed);
+                               
+                            if (keyPressed.Key == ConsoleKey.J)
+                            {
+                                // create booking...
+
+                                CreateBooking(
+                                    firstName,
+                                    lastName,
+                                    socialSecuirtyNumber,
+                                    phoneNumber,
+                                    email,
+                                    reason,
+                                    employee,
+                                    date);
+
+                                isBooked = true;
+
+                            }   
+                            
+
+                        } while (!isBooked);
+
+                        Clear();
+
+                        WriteLine("Booking skappad");
+
+                        Thread.Sleep(2000);
+
+                        Clear();
 
                         break;
 
                     case ConsoleKey.D2:
                     case ConsoleKey.NumPad2:
+
+                        ListBooking();
 
                         break;
 
@@ -78,6 +131,29 @@ namespace DentalCare
                 }
 
             }
+        }
+
+        private static void ListBooking()
+        {
+            WriteLine("Patient      Anledning         Tid");
+            WriteLine("--------------------------------------");
+
+            foreach (Booking booking in bookingList)
+            {
+                WriteLine($"{booking.FirstName} {booking.LastName}, {booking.SocialSecuirtyNumber}          {booking.Reason}  {booking.Date}");
+            }
+
+            ReadKey(true);
+
+            Clear();
+        }
+
+        private static void CreateBooking(string firstName, string lastName, string socialSecuirtyNumber, string phoneNumber, string email, string reason, string employee, DateTime date)
+        {
+            Booking booking = new Booking(firstName, lastName, socialSecuirtyNumber, phoneNumber, email,reason,employee, date);
+
+            bookingList.Add(booking);
+
         }
     }
 }
